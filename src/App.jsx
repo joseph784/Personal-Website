17a95b55ec38babcced0aeeabe97bb6d2b7fd5be
Joseph <josepha7@students.cs.ubc.ca>
2023-08-18 16:26:19 -0700

    Added gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
 import './App.css';
-import { useMemo, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './components/Home/HomePage'
-import AboutPage from './components/About/AboutPage';
-import WorkPage from './components/Work/WorkPage';
-import TabNavigation from './components/TabNavigation';
 
 export default function App() {
-
-
-
   return (
     <>
       <HomePage />
